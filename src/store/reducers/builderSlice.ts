782b64bf234389d6d5223b970931533c8ab7a467
Islam <islamibakaev@gmail.com>
2021-12-05 15:57:11 +0300

    refactoring redux code, part 5, started builderSlice

---
+++ src/store/reducers/builderSlice.ts
@@ -0,0 +1,49 @@
+import axios from 'axios'
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState, AppThunk } from '../../app/store';
+import { Ingredients } from '../actions/types';
+
+export interface BuilderState {
+  ingredients: Ingredients | null,
+  totalPrice: number,
+  error: boolean,
+}
+
+export interface ResPayload {
+  data: {
+    ingredients: Ingredients
+  }
+}
+
+const initialState: BuilderState = {
+  ingredients: null,
+  totalPrice: 0,
+  error: false,
+}
+
+export const fetchIngredients = createAsyncThunk(
+  'builder/fetchIngredients',
+  async () => {
+    try {
+      const response: ResPayload = await axios.get('https://react-burger-d4ed6.firebaseio.com/ingredients.json')
+      // dispatch(setIngredients(response.data.ingredients))
+      return response.data
+    } catch (error) {
+      // dispatch(fetchIngredientsFailed())
+  }
+)
+
+export const builderSlice = createSlice({
+  name: 'builder',
+  initialState,
+  reducers: {
+  },
+  extraReducers: (builder) => {
+    builder.
+      addCase(fetchIngredients.fulfilled, (state, action) => {
+        state.ingredients = action.payload.ingredients
+        state.totalPrice = 0
+        state.error = false
+      })
+  }
+})
